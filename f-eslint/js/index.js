5eeb7d1962f5ff992c9ed1546d9ce1ec33ae0c9c
CUL Web Team <ucs-webteam@city.ac.uk>
2019-03-07 10:59:09 +0000

    03/07/19 10:59:07 | ESL reports JQuery not used here. Tested on: https://www.cass.city.ac.uk/study/masters

--- f-eslint/js/index.js
+++ f-eslint/js/index.js
@@ -22560,10 +22560,6 @@ var className = 'cookie-notice';
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
-/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);
-
-
 
 
 __webpack_require__(/*! devbridge-autocomplete */ "./node_modules/devbridge-autocomplete/dist/jquery.autocomplete.js");
