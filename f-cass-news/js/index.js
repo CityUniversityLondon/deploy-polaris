68f99fac96b8a93eb55a3f97b884c12479fd84a6
CUL Web Team <ucs-webteam@city.ac.uk>
2019-07-25 15:10:23 +0000

    07/25/19 15:10:19 | JS: finished off slider

--- f-cass-news/js/index.js
+++ f-cass-news/js/index.js
@@ -22195,6 +22195,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_soundwave_bg_soundwave_bg__WEBPACK_IMPORTED_MODULE_24__ = __webpack_require__(/*! ./components/soundwave-bg/soundwave-bg */ "./src/components/soundwave-bg/soundwave-bg.js");
 /* harmony import */ var _components_carousel_carousel__WEBPACK_IMPORTED_MODULE_25__ = __webpack_require__(/*! ./components/carousel/carousel */ "./src/components/carousel/carousel.js");
 /* harmony import */ var _components_news_news__WEBPACK_IMPORTED_MODULE_26__ = __webpack_require__(/*! ./components/news/news */ "./src/components/news/news.js");
+/* harmony import */ var _components_slider_slider__WEBPACK_IMPORTED_MODULE_27__ = __webpack_require__(/*! ./components/slider/slider */ "./src/components/slider/slider.js");
 
 
 
@@ -22224,7 +22225,8 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-/* harmony default export */ __webpack_exports__["default"] = ([_components_cookie_notice_cookie_notice__WEBPACK_IMPORTED_MODULE_0__["default"], _components_accordion_tabs_accordion__WEBPACK_IMPORTED_MODULE_6__["default"], _components_accordion_tabs_tabs__WEBPACK_IMPORTED_MODULE_8__["default"], _components_accordion_tabs_accordion_tabs__WEBPACK_IMPORTED_MODULE_7__["default"], _components_video_preview_video_preview__WEBPACK_IMPORTED_MODULE_3__["default"], _components_popup_def_list_group_popup_def_list_group__WEBPACK_IMPORTED_MODULE_9__["default"], _components_url_dropdown_url_dropdown__WEBPACK_IMPORTED_MODULE_1__["default"], _components_url_slide_url_slide__WEBPACK_IMPORTED_MODULE_2__["default"], _components_nav_mobile__WEBPACK_IMPORTED_MODULE_4__["default"], _components_site_search__WEBPACK_IMPORTED_MODULE_5__["default"], _components_kis_widget_kis_widget__WEBPACK_IMPORTED_MODULE_10__["default"], _components_feature_news_feature_news__WEBPACK_IMPORTED_MODULE_12___default.a, _components_primary_dropdown_primary_dropdown__WEBPACK_IMPORTED_MODULE_11__["default"], _components_scroll_animations__WEBPACK_IMPORTED_MODULE_13__["default"], _components_action_on_scroll__WEBPACK_IMPORTED_MODULE_14__["default"], _components_video_banner_video_banner__WEBPACK_IMPORTED_MODULE_15__["default"], _components_video_banner_video_gradient_video_gradient__WEBPACK_IMPORTED_MODULE_16__["default"], _components_anchor_ghost__WEBPACK_IMPORTED_MODULE_17__["default"], _components_left_hand_navigation__WEBPACK_IMPORTED_MODULE_18__["default"], _components_course_search__WEBPACK_IMPORTED_MODULE_19__["default"], _components_banner_banner_stats__WEBPACK_IMPORTED_MODULE_20__["default"], _components_timeline_timeline__WEBPACK_IMPORTED_MODULE_21__["default"], _components_mba_popup_mba_popup__WEBPACK_IMPORTED_MODULE_22__["default"], _components_filter_dropdown_filter_dropdown__WEBPACK_IMPORTED_MODULE_23__["default"], _components_carousel_carousel__WEBPACK_IMPORTED_MODULE_25__["default"], _components_news_news__WEBPACK_IMPORTED_MODULE_26__["default"], _components_soundwave_bg_soundwave_bg__WEBPACK_IMPORTED_MODULE_24__["default"]]);
+
+/* harmony default export */ __webpack_exports__["default"] = ([_components_cookie_notice_cookie_notice__WEBPACK_IMPORTED_MODULE_0__["default"], _components_accordion_tabs_accordion__WEBPACK_IMPORTED_MODULE_6__["default"], _components_accordion_tabs_tabs__WEBPACK_IMPORTED_MODULE_8__["default"], _components_accordion_tabs_accordion_tabs__WEBPACK_IMPORTED_MODULE_7__["default"], _components_video_preview_video_preview__WEBPACK_IMPORTED_MODULE_3__["default"], _components_popup_def_list_group_popup_def_list_group__WEBPACK_IMPORTED_MODULE_9__["default"], _components_url_dropdown_url_dropdown__WEBPACK_IMPORTED_MODULE_1__["default"], _components_url_slide_url_slide__WEBPACK_IMPORTED_MODULE_2__["default"], _components_nav_mobile__WEBPACK_IMPORTED_MODULE_4__["default"], _components_site_search__WEBPACK_IMPORTED_MODULE_5__["default"], _components_kis_widget_kis_widget__WEBPACK_IMPORTED_MODULE_10__["default"], _components_feature_news_feature_news__WEBPACK_IMPORTED_MODULE_12___default.a, _components_primary_dropdown_primary_dropdown__WEBPACK_IMPORTED_MODULE_11__["default"], _components_scroll_animations__WEBPACK_IMPORTED_MODULE_13__["default"], _components_action_on_scroll__WEBPACK_IMPORTED_MODULE_14__["default"], _components_video_banner_video_banner__WEBPACK_IMPORTED_MODULE_15__["default"], _components_video_banner_video_gradient_video_gradient__WEBPACK_IMPORTED_MODULE_16__["default"], _components_anchor_ghost__WEBPACK_IMPORTED_MODULE_17__["default"], _components_left_hand_navigation__WEBPACK_IMPORTED_MODULE_18__["default"], _components_course_search__WEBPACK_IMPORTED_MODULE_19__["default"], _components_banner_banner_stats__WEBPACK_IMPORTED_MODULE_20__["default"], _components_timeline_timeline__WEBPACK_IMPORTED_MODULE_21__["default"], _components_mba_popup_mba_popup__WEBPACK_IMPORTED_MODULE_22__["default"], _components_filter_dropdown_filter_dropdown__WEBPACK_IMPORTED_MODULE_23__["default"], _components_carousel_carousel__WEBPACK_IMPORTED_MODULE_25__["default"], _components_news_news__WEBPACK_IMPORTED_MODULE_26__["default"], _components_soundwave_bg_soundwave_bg__WEBPACK_IMPORTED_MODULE_24__["default"], _components_slider_slider__WEBPACK_IMPORTED_MODULE_27__["default"]]);
 
 /***/ }),
 
@@ -25193,6 +25195,106 @@ function launch(el) {
   onToggle();
 }
 
+/* harmony default export */ __webpack_exports__["default"] = ({
+  launch: launch,
+  className: className
+});
+
+/***/ }),
+
+/***/ "./src/components/slider/slider.js":
+/*!*****************************************!*\
+  !*** ./src/components/slider/slider.js ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);
+
+
+
+
+function launch(el) {
+  console.log('launched');
+  var viewportWidth = jquery__WEBPACK_IMPORTED_MODULE_0___default()(window).width();
+  var slideCount = jquery__WEBPACK_IMPORTED_MODULE_0___default()('.news-articles-list li').length;
+  var slides = jquery__WEBPACK_IMPORTED_MODULE_0___default()('.news-articles-list li');
+  var strTouchX;
+  var endTouchX;
+
+  if (viewportWidth <= 900 && slideCount > 0) {
+    // detects slide direction. 5px movement deadzone has been allowed for swipe sensitivity. Slider will only react to swipes bigger than 5px in length 
+    var slideDirection = function slideDirection(endTouchX) {
+      endTouchX > strTouchX + 5 ? slide('+') : endTouchX < strTouchX - 5 ? slide('-') : null;
+    };
+
+    // handels slide
+    var slide = function slide(dir) {
+      var slidePosition = parseInt(jquery__WEBPACK_IMPORTED_MODULE_0___default()(".slider-news ul").css('left'));
+      var slideWidth = parseInt(jquery__WEBPACK_IMPORTED_MODULE_0___default()('.news-articles-list li').css('width'));
+      var animated = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".slider-news ul:animated").length;
+
+      if (dir === '-' && slidePosition - slideWidth >= -(slideWidth * (slideCount - 1)) && animated === 0) {
+        jquery__WEBPACK_IMPORTED_MODULE_0___default()(".slider-news ul").animate({
+          left: '' + dir + '=' + slideWidth + ''
+        }, 'slow', indicatorPosition(slidePosition - slideWidth, slideWidth));
+      } else if (dir === '+' && slidePosition + slideWidth <= 0 && animated === 0) {
+        jquery__WEBPACK_IMPORTED_MODULE_0___default()(".slider-news ul").animate({
+          left: '' + dir + '=' + slideWidth + ''
+        }, 'slow', indicatorPosition(slidePosition + slideWidth, slideWidth));
+      }
+    };
+
+    // highlights which slide you on
+    var indicatorPosition = function indicatorPosition(value, slideWidth) {
+      var slideInd = value / -slideWidth;
+      jquery__WEBPACK_IMPORTED_MODULE_0___default()('.slider__indicators__indicator').each(function () {
+        if (jquery__WEBPACK_IMPORTED_MODULE_0___default()(this).index() == slideInd) {
+          jquery__WEBPACK_IMPORTED_MODULE_0___default()(this).addClass('slider__indicators__indicator--active');
+        } else {
+          jquery__WEBPACK_IMPORTED_MODULE_0___default()(this).removeClass('slider__indicators__indicator--active');
+        }
+      });
+    };
+
+    // implements slider
+    //$('.news-articles-list').before('<div class="slider"></div>');
+    var slider = jquery__WEBPACK_IMPORTED_MODULE_0___default()('.slider-news'); //$('.news-articles-list').appendTo($('.slider'));
+    // generates slide indicators
+
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()('.slider-news ul').after('<div class="slider__indicators__wrap"><div class="slider__indicators"></div></div>');
+    slides.each(function () {
+      jquery__WEBPACK_IMPORTED_MODULE_0___default()('.slider__indicators').append('<div class="slider__indicators__indicator"></div>');
+    });
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()('.slider__indicators__indicator').eq(0).addClass('slider__indicators__indicator--active'); // records touches
+
+    slider.on('touchstart', function (e) {
+      strTouchX = e.touches[0].clientX;
+    });
+    slider.on('touchend', function (f) {
+      endTouchX = f.changedTouches[0].clientX;
+      slideDirection(endTouchX);
+    });
+    ;
+    ; // slide index indicators
+
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()('.slider__indicators__indicator').on('click', function () {
+      var slideWidth = parseInt(jquery__WEBPACK_IMPORTED_MODULE_0___default()('.news-articles-list li').css('width'));
+      var slideIndex = jquery__WEBPACK_IMPORTED_MODULE_0___default()(this).index();
+      var slidePosition = slideWidth * slideIndex;
+      jquery__WEBPACK_IMPORTED_MODULE_0___default()(".slider-news ul").animate({
+        left: -slidePosition
+      }, 'slow', indicatorPosition(slidePosition, -slideWidth));
+    });
+    ;
+  } // end if
+
+}
+
+var className = 'slider-news';
 /* harmony default export */ __webpack_exports__["default"] = ({
   launch: launch,
   className: className
